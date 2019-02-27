6ff3494998ba553fea0501fb90e82d6437a73dc4
unknown <Student@A125534.ad.csueastbay.edu>
2019-02-27 11:32:13 -0800

    Change

--- src/lab6/Lab6.java
+++ src/lab6/Lab6.java
@@ -17,6 +17,7 @@ public class Lab6 {
     public static void main(String[] args) {
             System.out.println("Hello");
                         System.out.println("Hello");
+                        System.out.println("Ricardo here");
 
 
     }
